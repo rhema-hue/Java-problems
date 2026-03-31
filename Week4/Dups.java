bee756231a63a401c95aa6b0e085e2a9a300e61c
rhema-hue <rhemapalanivel@gmail.com>
2026-03-31 22:50:55 +0530

    Add files via upload

---
+++ Week4/Dups.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Dups {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[]arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        Arrays.sort(arr);
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i]==arr[j]){
+                    System.out.print(arr[i]+" ");
+                }
+            }
+        }
+    }
+}
